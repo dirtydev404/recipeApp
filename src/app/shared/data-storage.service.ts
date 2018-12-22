6ef1bdd91c36f17ecf22485e0d0d6782ea23ae0a
Thad <thadlounds@gmail.com>
2018-12-21 19:46:14 -0700

    working on server connection

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+    storeRecipes() {
+     return this.http.put('https://ng-recipe-app-afbb1.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes());
+    }
+}
