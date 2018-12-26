b18cbead2152292ba48a261389edc0a0162e6944
Thad <thadlounds@gmail.com>
2018-12-26 13:08:59 -0700

    working on cleaning up modules

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate() {
   constructor(private authService: AuthService) {}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.authService.isAuthenticated();
