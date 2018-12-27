57671001384ac3ce554b3b208d24f040a66fcc20
Thad <thadlounds@gmail.com>
2018-12-27 12:13:22 -0700

    finished modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/home/home.component';
 
 const AppRoutes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 @NgModule({
